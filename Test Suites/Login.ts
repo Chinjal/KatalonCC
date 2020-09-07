657a8d165e29d1f196d48637fe71b874fc955d33
ChinjalModi <ChinjalModi@DESKTOP-ML2QQGB>
2020-09-07 19:56:49 +0530

    commited change

--- Test Suites/Login.ts
+++ Test Suites/Login.ts
@@ -10,4 +10,10 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>793a22fd-76c1-4542-a4d2-d07f255832fb</testSuiteGuid>
+   <testCaseLink>
+      <guid>d2af3b6c-ee34-461b-92ed-bca1ab53c5c7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 1</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
