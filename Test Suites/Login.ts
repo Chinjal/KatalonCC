b7bc5befd125babbd450e213f13bd4e765b26d20
ChinjalModi <ChinjalModi@DESKTOP-ML2QQGB>
2020-09-07 12:21:54 +0530

    Test suit added

---
+++ Test Suites/Login.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>793a22fd-76c1-4542-a4d2-d07f255832fb</testSuiteGuid>
+</TestSuiteEntity>
